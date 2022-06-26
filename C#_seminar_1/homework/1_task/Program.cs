d234e2bde078dc1de80886c2e16581655070dc53
lissa1 <107270748+lissa1@users.noreply.github.com>
2022-06-26 19:55:20 +0700

    Add files via upload

---
+++ C#_seminar_1/homework/1_task/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("ввeдите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ввeдите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("max = " + number1);
+    Console.WriteLine("min = " + number2);
+}
+else if (number1 == number2)
+{
+  Console.WriteLine($"числа равны {number1} = {number2}");
+}
+else
+{
+    Console.WriteLine("max = " + number2);
+    Console.WriteLine("min = " + number1);
+}
---
+++ C#_seminar_1/homework/2_task/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("ввeдите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ввeдите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("ввeдите третье число ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max > number2)
+{
+    if (max > number3)
+    {
+        max = number1;
+    }
+    else
+    {
+        max = number2;
+    }
+
+}
+else
+{
+    if (number2 > number3)
+    {
+        max = number2;
+    }
+    else 
+    {
+        max = number3;
+    }
+}
+Console.WriteLine("max = " + max);
